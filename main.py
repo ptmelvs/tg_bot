c362ad2278ccea2f88ac431dea619c95860f6dfb
Nikita Kapusta <zgadena2@gmail.com>
2023-05-07 23:10:35 +0600

    Make first working option

--- main.py
+++ main.py
@@ -7,28 +7,52 @@ from googletrans import Translator
 translator = Translator()
 list_of_langs = list(googletrans.LANGUAGES)
 avl_langs = ['EN🇬🇧', 'KO🇰🇷', 'ZH-CN🇨🇳', 'FR🇲🇫', 'DE🇩🇪']
+
+
 def read_token() -> str:
     with open('C:\\Users\\ch991\\PycharmProjects\\tg_bot\\venv\\token.txt', ) as file:
         token = file.read().removesuffix('\n')
         return token
 
-src = ''
-dest = ''
+
+dest = 'ru'
 
 API_TOKEN = read_token()
 bot = telebot.TeleBot(API_TOKEN)
+
+
 @bot.message_handler(commands=['tolangs'])
 def av_langs(message):
     markup = types.InlineKeyboardMarkup()
-    button1 = types.InlineKeyboardButton(text=avl_langs[0], callback_data='butt_id')
-    button2 = types.InlineKeyboardButton(text=avl_langs[1], callback_data='butt_id')
-    button3 = types.InlineKeyboardButton(text=avl_langs[2], callback_data='butt_id')
-    button4 = types.InlineKeyboardButton(text=avl_langs[3], callback_data='butt_id')
-    button5 = types.InlineKeyboardButton(text=avl_langs[4], callback_data='butt_id')
+    button1 = types.InlineKeyboardButton(text=avl_langs[0], callback_data='butt_idE')
+    button2 = types.InlineKeyboardButton(text=avl_langs[1], callback_data='butt_idK')
+    button3 = types.InlineKeyboardButton(text=avl_langs[2], callback_data='butt_idZC')
+    button4 = types.InlineKeyboardButton(text=avl_langs[3], callback_data='butt_idF')
+    button5 = types.InlineKeyboardButton(text=avl_langs[4], callback_data='butt_idD')
 
     markup.add(button1, button2, button3, button4, button5)
 
     bot.send_message(message.chat.id, 'Доступные языки для перевода:', reply_markup=markup)
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def check_callback(call):
+    global src
+    if call.data == 'butt_idE':
+        bot.send_message(call.message.chat.id, 'Вы выбрали английский язык, введите слово или предложение на английском, которые вы хотите перевести')
+        src = 'en'
+    elif call.data == 'butt_idK':
+        bot.send_message(call.message.chat.id, 'Вы выбрали ко   рейский язык, введите слово или предложение на корейском, которые вы хотите перевести')
+        src = 'ko'
+    elif call.data == 'butt_idZC':
+        bot.send_message(call.message.chat.id, 'Вы выбрали китайский язык, введите слово или предложение на китайском, которые вы хотите перевести')
+        src = 'zh-cn'
+    elif call.data == 'butt_idF':
+        bot.send_message(call.message.chat.id, 'Вы выбрали французский язык, введите слово или предложение на французском, которые вы хотите перевести')
+        src = 'fr'
+    elif call.data == 'butt_idD':
+        bot.send_message(call.message.chat.id, 'Вы выбрали немецкий язык, введите слово или предложение на немецком, которые вы хотите перевести')
+        src = 'de'
 
 
 @bot.message_handler(func=lambda m: True)
